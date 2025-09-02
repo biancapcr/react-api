13875f8b46b1fc905da7b5afdedf975103f6ef11
biancapcr <biancacristina.pocora@gmail.com>
2025-09-02 21:26:42 +0200

    [feat]: implement return

--- src/App.jsx
+++ src/App.jsx
@@ -16,30 +16,34 @@ function App() {
   useEffect(fetchActresses, []);
 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+return (
+  <div className="wrapper">
+    <h1 className="title">Lista Attrici</h1>
+
+    <div className="grid">
+      {actresses.map((actress) => (
+        <div className="grid-item" key={actress.id}>
+          <div className="card">
+            <img
+              src={actress.image}
+              className="card-image card-img"
+              alt={actress.name}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{actress.name}</h5>
+              <p className="card-text">{actress.biography}</p>
+            </div>
+            <ul className="details">
+              <li className="details-row bg-1">{actress.birth_year}</li>
+              <li className="details-row bg-2">{actress.nationality}</li>
+              <li className="details-row bg-3">{actress.awards}</li>
+            </ul>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 }
 
-export default App
+export default App;
